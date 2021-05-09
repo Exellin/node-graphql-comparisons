a0c1f074fe913945f57c34a4b94036510a9280dc
Shawn Carnegie <carnegieshawn@gmail.com>
2021-05-09 08:22:40 -0600

    Add apollo-server-express Objection Graphql-js GraphQL-code-generator setup

---
+++ entrypoints/apollo-server-express-Graphql-js-Objection-GraphQL-code-generator/models/User.ts
@@ -0,0 +1,16 @@
+import { Model } from 'objection';
+
+import { User as graphqlUserType } from '../generated/graphql';
+
+interface User extends Omit<graphqlUserType, 'name'> {}
+
+class User extends Model {
+  static tableName = 'user';
+  password: string
+
+  name(): string {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+export default User;
---
+++ entrypoints/apollo-server-express-Graphql-js-Objection-GraphQL-code-generator/schema/user.ts
@@ -0,0 +1,28 @@
+import {
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString
+} from "graphql";
+
+const userGraphqlType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    firstName: {
+      type: GraphQLString
+    },
+    lastName: {
+      type: GraphQLString
+    },
+    email: {
+      type: GraphQLString
+    },
+    name: {
+      type: GraphQLString
+    }
+  }
+});
+
+export default userGraphqlType;
